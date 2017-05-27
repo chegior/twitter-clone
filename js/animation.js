77988d439057d5be2362165bceee035adff7a9ad
chegior <chegiorca@hotmail.com>
2017-05-27 10:52:49 -0600

    Part One 100%

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
+
+
+//////////OBJECT TWEET///////////////
+
+/////////////////////////////////////
+
+
+
   $('#tweet-controls').hide();
   //the Tweet button and the character count button should be hidden
   $('.tweet-compose').on('focus',function(){
     $('#tweet-controls').show();
     $(this).css('height', '5em');
 // focus: when clicks on the text area
+  //  var tweetClone = $('.tweet').first().clone();
 
+//    tweetClone.find('.avatar').attr('src', '')
     var wordCounter = 140;
     $(this).keyup(function(){
       var typeText = $(this).val().length;
@@ -26,7 +36,30 @@ $(document).ready(function(){
       }
     });
 
+
+
   });
+
+  $('#tweet-submit').on('click',function(){
+    ////////////add on STREAM////////////
+    var newTweetCont = $('.tweet').first().clone();//save the CONTEXT of a tweet
+    var image = $('#profile-summary').find('.avatar').attr('src');
+    var name = 'chedro';
+    var textArea = $('.tweet-compose').val();
+    console.log(image);
+    newTweetCont.find('.fullname').html(name);
+    newTweetCont.find('.avatar').attr('src',image);
+    newTweetCont.find('.tweet-text').html(textArea);
+
+    $("#stream").prepend(newTweetCont);
+
+
+
+
+
+
+  });
+
 
 
 
